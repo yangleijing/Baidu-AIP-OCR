836f27c784d4ec63c14f5bd035145cec8a977f6f
yangleijing <49393689+yangleijing@users.noreply.github.com>
2019-05-14 09:26:37 +0800

    delphi 调取百度OCR 文字识别
    1、将 BaiduORC.tlb 加入到delphi 中，会生成一个com类

---
+++ demo/DemoAipOcr.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+<?php
+//如果用JSON格式则要使用text/html，不能使用text/xml
+
+// 引入文字识别OCR SDK
+require_once '../AipOcr.php';
+
+// 定义常量
+const APP_ID = '10526644';
+const API_KEY = 'MMWDEI3QnGBqwW9QY5Wfw8x0';
+const SECRET_KEY = 'Fv4jFgCkHtlAbDa11t5PHlRXDIgpoK4k';
+
+
+// 初始化
+$aipOcr = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
+
+// 身份证识别
+ // echo urldecode(json_encode($aipOcr->idcard(file_get_contents('idcard.jpg'), true),JSON_PRETTY_PRINT));
+ $str= json_encode($aipOcr->idcard(file_get_contents('idcard.jpg'), true),JSON_PRETTY_PRINT);
+  preg_replace("#\\\u([0-9a-f]{4})#ie", "iconv('UCS-2LE', 'UTF-8', pack('H4', '\\1'))", $str);
+ var_dump($str);
+
+// 银行卡识别 
+// echo json_encode($aipOcr->bankcard(file_get_contents('bankcard.jpg')));
+
+// 通用文字识别(含文字位置信息)
+// echo json_encode($aipOcr->general(file_get_contents('general.png')));
+
+
+// 通用文字识别(不含文字位置信息)
+// echo json_encode($aipOcr->basicGeneral(file_get_contents('general.png')), JSON_PRETTY_PRINT);
+
+// 网图OCR识别
+// echo json_encode($aipOcr->webImage(file_get_contents('general.png')), JSON_PRETTY_PRINT);
+
+// 生僻字OCR识别
+// echo json_encode($aipOcr->enhancedGeneral(file_get_contents('general.png')), JSON_PRETTY_PRINT);
+
+// 行驶证识别
+// echo json_encode($aipOcr->vehicleLicense(file_get_contents('vehicleLicense.jpg')), JSON_PRETTY_PRINT);
+
+// 驾驶证
+// echo json_encode($aipOcr->drivingLicense(file_get_contents('drivingLicense.jpg')), JSON_PRETTY_PRINT);
+
+// 车牌
+//echo json_encode($aipOcr->licensePlate(file_get_contents('licensePlate.jpg')), JSON_PRETTY_PRINT);
+?>
+</body>
+</html>
